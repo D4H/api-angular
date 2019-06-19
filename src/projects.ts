18138db5e27f98f72fef12dd06c6fbaba7cd1a6c
Mark <mark@bhalash.com>
2019-06-19 17:58:58 +0100

    Added testing utilities to exports.

--- src/projects.ts
+++ src/projects.ts
@@ -1,5 +1,18 @@
+/**
+ * Client, Models and Services
+ * =============================================================================
+ */
+
 export * from './lib/client.module';
 export * from './lib/models';
 export * from './lib/providers';
 export * from './lib/routes';
 export * from './lib/services';
+
+/**
+ * Testing Utilities
+ * =============================================================================
+ */
+
+export * from './spec/factories';
+export * from './spec/utilities';
