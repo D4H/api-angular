a1054c6346d525fefd37ba5bf394e89c0daef86a
Mark <mark@bhalash.com>
2019-06-21 00:29:54 +0100

    Added Range interface.
    This is used extensively in the Personnel calendar.

--- src/lib/models/period.model.ts
+++ src/lib/models/period.model.ts
@@ -1,13 +1,18 @@
-
 /**
- * Model Period Attributes
+ * Periods and Ranges
  * =============================================================================
- * Period is used in the attributes of records with a start/end date. Duration
- * is sometimes present as minutes between date and enddate.
+ *
+ *  1. Period is used in the attributes of records with a start/end date.
+ *  2. Range is used to query the start/end date in the API.
  */
 
 export interface Period {
   date: Date;
   enddate: Date;
   duration?: number;
+}
+
+export interface Range {
+  after: Date;
+  before: Date;
 }
