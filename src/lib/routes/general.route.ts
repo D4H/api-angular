d196479fe93dc4055b10bc290959e65e2f6459b1
Mark <mark@bhalash.com>
2019-06-21 14:19:38 +0100

    Merged Errors and exported top-level interfaces.
    1. Merged Error* interfaces into general file.
    2. Exported general interfaces under API* namespace.

--- src/lib/routes/general.route.ts
+++ src/lib/routes/general.route.ts
@@ -69,3 +69,41 @@ export interface Response<T> {
   statusCode: number;
   meta?: Page;
 }
+
+/**
+ * Object Model Errors
+ * =============================================================================
+ * On invalid POST/PUT.
+ *
+ * @see: https://github.com/D4H/decisions-project/issues/2521
+ */
+
+export interface ModelError {
+  constraint: string;
+  key: string;
+  message: string;
+  path: string;
+  type: string;
+}
+
+/**
+ * API Error Responses
+ * =============================================================================
+ * Error codes: 400, 403, 404, 422.
+ */
+
+export interface Error {
+  error: string;
+  message: string;
+  statusCode: number;
+
+  meta: {
+    reference: string;
+  };
+
+  validation?: {
+    errors: Array<ModelError>;
+    keys: Array<string>;
+    source: string;
+  };
+}
--- src/lib/routes/index.ts
+++ src/lib/routes/index.ts
@@ -1,8 +1,8 @@
+import * as API from './general.route';
 import * as Accounts from './account.route';
 import * as Activities from './activity.route';
 import * as Attendances from './attendance.route';
 import * as Duties from './duty.route';
-import * as Errors from './error.route';
 import * as Groups from './group.route';
 import * as Members from './member.route';
 import * as Notes from './note.route';
@@ -11,11 +11,11 @@ import * as Roles from './role.route';
 import * as Teams from './team.route';
 
 export {
+  API,
   Accounts,
   Activities,
   Attendances,
   Duties,
-  Errors,
   Groups,
   Members,
   Notes,
