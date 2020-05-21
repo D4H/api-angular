73181de6273b8655076ffbbdb1a46f34523c04ae
Mark Grealish <mark@bhalash.com>
2020-05-21 22:02:29 +0200

    refactor(client): change EntityType enum to type
    The enum was painful to use in practice.
    
    See: d4h/decisions-project#4358

--- src/lib/models/entity-type.model.ts
+++ src/lib/models/entity-type.model.ts
@@ -12,75 +12,74 @@
  * @see https://github.com/D4H/decisions-web-app/blob/16c104aefac9b0518894322101847f74c00cfa42/app/Library/ObjectDefinition/EntityTypeEnum.php#L151-L694
  */
 
-export enum EntityType {
-  Account = 'account',
-  Activity = 'activity',
-  Admin = 'admin',
-  Animal = 'animal',
-  AnimalQualification = 'animal-qualification',
-  Attendance = 'member2activity',
-  Audit = 'audit',
-  Capability = 'capability',
-  Bundle = 'bundle',
-  Casualty = 'casualty',
-  ChatItem = 'chat_item',
-  Company = 'company',
-  Contact = 'contact',
-  Costing = 'costing',
-  CustomField = 'custom_field',
-  CustomReport = 'report',
-  Division = 'division',
-  Document = 'document',
-  Duty = 'duty',
-  EntityBundle = 'entity_bundle',
-  Event = 'event',
-  Exercise = 'exercise',
-  Folder = 'folder',
-  Gear = 'gear',
-  GearBrand = 'gear_brand',
-  GearCategory = 'gear_category',
-  GearFund = 'gear_fund',
-  GearKind = 'gear_type',
-  GearKit = 'gear_kit',
-  GearLocation = 'gear_location',
-  GearModel = 'gear_model',
-  GearMove = 'gear_move',
-  GearRepair = 'repair',
-  GearRetiredReason = 'gear_retired_reason',
-  Group = 'group',
-  Handler = 'handler',
-  HandlerQualification = 'handler-qualification',
-  HazmatContainerType = 'hazmat_container_type',
-  HazmatIncidentType = 'hazmat_incident_type',
-  HazmatLabStatus = 'hazmat_lab_status',
-  HazmatLabType = 'hazmat_lab_type',
-  HazmatOutcome = 'hazmat_outcome',
-  HazmatMaterial = 'hazmat_material',
-  HazmatPropertyType = 'hazmat_property_type',
-  HealthSafetyCategory = 'healthsafety_cat',
-  HealthSafetyReport = 'healthsafety_report',
-  HealthSafetySeverity = 'healthsafety_sev',
-  Incident = 'incident',
-  IndexRecord = 'index',
-  Inspection = 'inspection',
-  InspectionResult = 'inspection2gear',
-  LocationBookmark = 'location_bookmark',
-  Member = 'member',
-  MemberQualification = 'member-qualification',
-  Officer = 'officer',
-  Organisation = 'organisation',
-  Person = 'person',
-  PersonInvolved = 'person_involved',
-  Course = 'course',
-  Resource = 'resource',
-  ResourceType = 'resource_type',
-  Role = 'role',
-  ShortUrl = 'short_url',
-  Supplier = 'supplier',
-  SuppliersRef = 'suppliers-ref',
-  Tag = 'tag',
-  Task = 'task',
-  Unit = 'unit',
-  Vehicle = 'vehicle',
-  Whiteboard = 'whiteboard'
-}
+export type EntityType
+  = 'account'
+  | 'activity'
+  | 'admin'
+  | 'animal'
+  | 'animal-qualification'
+  | 'member2activity'
+  | 'audit'
+  | 'capability'
+  | 'bundle'
+  | 'casualty'
+  | 'chat_item'
+  | 'company'
+  | 'contact'
+  | 'costing'
+  | 'custom_field'
+  | 'report'
+  | 'division'
+  | 'document'
+  | 'duty'
+  | 'entity_bundle'
+  | 'event'
+  | 'exercise'
+  | 'folder'
+  | 'gear'
+  | 'gear_brand'
+  | 'gear_category'
+  | 'gear_fund'
+  | 'gear_type'
+  | 'gear_kit'
+  | 'gear_location'
+  | 'gear_model'
+  | 'gear_move'
+  | 'repair'
+  | 'gear_retired_reason'
+  | 'group'
+  | 'handler'
+  | 'handler-qualification'
+  | 'hazmat_container_type'
+  | 'hazmat_incident_type'
+  | 'hazmat_lab_status'
+  | 'hazmat_lab_type'
+  | 'hazmat_outcome'
+  | 'hazmat_material'
+  | 'hazmat_property_type'
+  | 'healthsafety_cat'
+  | 'healthsafety_report'
+  | 'healthsafety_sev'
+  | 'incident'
+  | 'index'
+  | 'inspection'
+  | 'inspection2gear'
+  | 'location_bookmark'
+  | 'member'
+  | 'member-qualification'
+  | 'officer'
+  | 'organisation'
+  | 'person'
+  | 'person_involved'
+  | 'course'
+  | 'resource'
+  | 'resource_type'
+  | 'role'
+  | 'short_url'
+  | 'supplier'
+  | 'suppliers-ref'
+  | 'tag'
+  | 'task'
+  | 'unit'
+  | 'vehicle'
+  | 'whiteboard';
